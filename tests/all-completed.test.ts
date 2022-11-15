5a4f54f910cfbb82c6eed8b92c54ef7bc237a7b1
Vladimir Semenov <vovsemenv@gmail.com>
2022-11-15 17:30:28 +0400

    add screen

--- tests/all-completed.test.ts
+++ tests/all-completed.test.ts
@@ -1,5 +1,9 @@
 import { test } from "@playwright/test";
 
-test("ff", async ({ page }) => {
+test("ff", async ({ page }, testInfo) => {
+  await testInfo.attach("basic-page-screen", {
+    body: await page.screenshot(),
+    contentType: "image/png",
+  });
   await page.locator("non-existing-locator").click();
 });
